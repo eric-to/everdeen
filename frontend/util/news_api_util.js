64749d8d130553f28bf842ce38dc1eb425d6b4d7
Eric To <erto@berkeley.edu>
2018-12-15 02:16:58 -0800

    Prevent sidebar from collapsing onto other page elements

--- frontend/util/news_api_util.js
+++ frontend/util/news_api_util.js
@@ -1,9 +1,9 @@
 export const fetchNews = () => {
-  const url = 'https://newsapi.org/v2/everything?' +
-              'q=Apple&' +
-              'from=2018-12-14&' +
-              'sortBy=popularity&' +
-              'pageSize=30&apiKey=' +
-              window.newsAPIKey;
-  return $.ajax(url)
+  // const url = 'https://newsapi.org/v2/everything?' +
+  //             'q=Apple&' +
+  //             'from=2018-12-14&' +
+  //             'sortBy=popularity&' +
+  //             'pageSize=30&apiKey=' +
+  //             window.newsAPIKey;
+  return $.ajax('https://newsapi.org/v2/top-headlines?sources=the-wall-street-journal&apiKey=a8591a0bdd2945bea9a91fb46d21dfda')
 }
