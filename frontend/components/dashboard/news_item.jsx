ffbb92f369ff857111fe1186de6aaa80cdee2720
Eric To <erto@berkeley.edu>
2019-01-23 09:55:30 -0800

    Refactor news item

--- frontend/components/dashboard/news_item.jsx
+++ frontend/components/dashboard/news_item.jsx
@@ -1,46 +1,43 @@
 import React from "react";
 
-const NewsItem = ({ newsItem }) => {
-  const MONTHS = {
-    "01": "Jan",
-    "02": "Feb",
-    "03": "Mar",
-    "04": "Apr",
-    "05": "May",
-    "06": "Jun",
-    "07": "Jul",
-    "08": "Aug",
-    "09": "Sep",
-    "10": "Oct",
-    "11": "Nov",
-    "12": "Dec"
-  }
+const MONTHS = {
+  "01": "Jan",
+  "02": "Feb",
+  "03": "Mar",
+  "04": "Apr",
+  "05": "May",
+  "06": "Jun",
+  "07": "Jul",
+  "08": "Aug",
+  "09": "Sep",
+  "10": "Oct",
+  "11": "Nov",
+  "12": "Dec"
+}
 
-  const timeAgo = (time) => {
-    const timeDiff = (new Date()) - (new Date(time));
-    const seconds = Math.floor(timeDiff / 1000);
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(minutes / 60);
-    if (seconds < 60) {
-      return `${seconds}s ago`;
-    } else if (minutes < 60) {
-      return `${minutes}m ago`;
-    } else if (hours < 24) {
-      return `${hours}hr ago`;
-    } else {
-      const date = time.slice(5, 10).split("-");
-      const month = MONTHS[date[0]];
-      const day = date[1];
-      if (month === undefined) {
-        console.log(date);
-      }
-      if (day[0] === "0") {
-        return `${month} ${day.slice(1)}`;
-      }
-      return `${month} ${day}`;
+const timeAgo = (time) => {
+  const timeDiff = (new Date()) - (new Date(time));
+  const seconds = Math.floor(timeDiff / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  if (seconds < 60) {
+    return `${seconds}s ago`;
+  } else if (minutes < 60) {
+    return `${minutes}m ago`;
+  } else if (hours < 24) {
+    return `${hours}hr ago`;
+  } else {
+    const date = time.slice(5, 10).split("-");
+    const month = MONTHS[date[0]];
+    const day = date[1];
+    if (day[0] === "0") {
+      return `${month} ${day.slice(1)}`;
     }
-  } 
+    return `${month} ${day}`;
+  }
+} 
 
+const NewsItem = ({ newsItem }) => {
   const description = newsItem.description.split(" ");
   let summary = "";
   for (let i = 0; i < description.length; i++) {
